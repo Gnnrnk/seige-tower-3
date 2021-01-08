5f96d2363bbaecdd91bfbf00a161f4cccfdddfeb
Gnnrnk <74908072+Gnnrnk@users.noreply.github.com>
2021-01-08 12:17:52 +0530

    Add files via upload

---
+++ Blue.js
@@ -0,0 +1,25 @@
+class Blue extends Baseclass{
+   constructor(x,y,width,height){
+       super(x,y,width,height)
+   }
+   display(){
+       fill(130,237,237);
+       if(this.box.speed<3){
+       super.display();
+   }
+   else{
+      
+       World.remove(world,this.box)
+       push();
+       this.visiblity = this.visiblity-5;
+       tint(255,this.visiblity);
+       pop();
+   }
+   }
+    
+    score(){
+        if(this.visiblity<0 && this.visiblity>-105){
+            score++;
+        }
+    }
+}
---
+++ Yellow.js
@@ -0,0 +1,25 @@
+class Yellow extends Baseclass{
+    constructor(x,y,width,height){
+        super(x,y,width,height)
+    }
+    display(){
+        fill(239,247,72)
+        if(this.box.speed<3){
+            super.display();
+        }
+        else{
+           
+            World.remove(world,this.box)
+            push();
+            this.visiblity = this.visiblity-5;
+            tint(255,this.visiblity)
+            pop();
+     
+        }
+    }
+    score(){
+        if(this.visiblity<0 && this.visiblity>-105){
+            score++;
+        }
+    }
+}
